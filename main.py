f999f3b00994c2ecc63050c1ec1d5af58c76f402
kisshere69 <kizzfromdahood@gmail.com>
2025-11-30 08:01:15 +0200

    changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 todos = []
 while True:
-    user_action = input("Type add/show/exit: ")
+    user_action = input("Type add/show/edit/exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,6 +10,12 @@ while True:
             todos.append(todo)
         case "show":
             print(todos)
+        case "edit":
+            todo_number = int(input("Select a todo to edit: "))
+            todo_number = todo_number - 1
+            new_todo = input("Enter a new todo: ")
+            todos[todo_number] = new_todo
+            print("New Todo Has Been Saved!")
         case "exit":
             break
         case user_error:
