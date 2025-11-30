c936f5ffc002e8b0c9bbdfa2e11747b23e62a665
kisshere69 <kizzfromdahood@gmail.com>
2025-11-30 07:45:10 +0200

    new material

---
+++ learning.py
@@ -0,0 +1,7 @@
+#Replace a symbol in a string
+
+filenames = ["1. Raw Data.txt", "2. Reports.txt", "3. Presentation.txt"]
+
+for filename in filenames:
+    filename = filename.replace('.', ')', 1)
+    print(filename)
