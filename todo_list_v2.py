bedb40483d4b0d52a27e12df3c318c4344ef5c0a
Nikita Kiss <155293675+kisshere69@users.noreply.github.com>
2025-12-07 19:24:34 +0200

    Add files via upload
    Try-except for error handling

--- todo_list_v2.py
+++ todo_list_v2.py
@@ -2,14 +2,14 @@ while True:
     #expect the user enter one of the actions
     user_action = input("Type add/show/replace/delete/exit: ").strip()
 
-    if "add" in user_action:
+    if user_action.startswith("add"):
             #expect the user to enter add and todo
             todo = user_action[4:]
             #open a file where this string will be stored and append the string to the end.
             with open("files/todos.txt", "a") as file:
                 file.write(todo)
 
-    elif "show" in user_action:
+    elif user_action.startswith("show"):
             #open a file and read the content by line
             with open("files/todos.txt", "r") as file:
                 todos = file.readlines()
@@ -19,40 +19,47 @@ while True:
                 item = item.strip("\n")
                 print(f"{index + 1}. {item.capitalize()}")
 
-    elif "replace" in user_action:
-        #expect the user to enter replace and index value
-        number = int(user_action[8:])
-        number = int(number) - 1
-        #open a file
-        with open("files/todos.txt", "r") as file:
-            todos = file.readlines()
+    elif user_action.startswith('replace'):
+        try:
+            number = int(user_action[5:]) - 1
 
-        #expect the user to enter a string for replacement
-        new_todo = input("Enter new todo: ") + "\n"
+            #open a file
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
 
-        #save the new string in the same place (index)
-        todos[number] = new_todo
-        print("New Todo Has Been Saved!")
+            #expect the user to enter a string for replacement
+            new_todo = input("Enter new todo: ")
 
-        #open a file and overwrite the string
-        with open("files/todos.txt", "w") as file:
-            file.writelines(todos)
+            #save the new string in the same place (index)
+            todos[number] = new_todo  + "\n"
+            print("New Todo Has Been Saved!")
 
-    elif "delete" in user_action:
+            #open a file and overwrite the string
+            with open("files/todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Invalid Input! Please try the command again.")
+            continue
+
+    elif user_action.startswith('delete'):
+        try:
         #expect the user to enter delete and index value
-        number = int(user_action[7:])
-        number = int(number) - 1
-        with open("files/todos.txt", "r") as file:
-            todos = file.readlines()
+            number = int(user_action[7:])
+            number = int(number) - 1
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
 
         #delete the index by the entered number. number value = index in a string
-        todos.pop(number)
+            todos.pop(number)
 
         #update the file
-        with open("files/todos.txt", "w") as file:
-            file.writelines(todos)
+            with open("files/todos.txt", "w") as file:
+                file.writelines(todos)
+        except IndexError:
+            print("Invalid Input! The todo with this number does not exist.")
+            continue
 
-    elif "exit" in user_action:
+    elif user_action.startswith('exit'):
         break
 
     else:
