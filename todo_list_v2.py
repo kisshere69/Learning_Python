2fe4adc257beec19edfc3778db006d0f135cf671
Nikita Kiss <155293675+kisshere69@users.noreply.github.com>
2025-12-08 08:40:14 +0200

    Add files via upload
    Upgraded ToDo_list_v2

--- todo_list_v2.py
+++ todo_list_v2.py
@@ -1,42 +1,54 @@
+def get_todos():
+    with open("files/todos.txt", "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos):
+    with open("files/todos.txt", "w") as file_local:
+        file_local.writelines(todos)
+
 while True:
-    #expect the user enter one of the actions
-    user_action = input("Type add/show/replace/delete/exit: ").strip()
+    #expect the user enter one of the actions in lowercase
+    user_action = input("Type add/show/replace/delete/exit: ").strip().lower()
 
     if user_action.startswith("add"):
-            #expect the user to enter add and todo
+            #expect the user to enter add and a string
             todo = user_action[4:]
-            #open a file where this string will be stored and append the string to the end.
-            with open("files/todos.txt", "a") as file:
-                file.write(todo)
+            #open a file where this string will be stored
+            todos = get_todos()
+
+            #check if the todo already exists
+            if todo.strip().lower() in [t.strip().lower() for t in todos]:
+                print(f"Todo '{todo.strip()}' already exists!")
+            else:
+                #append the string to the end of the list.
+                print(f"Todo '{todo.strip()}' has been added.")
+                todos.append(todo + "\n")
+                write_todos(todos)
 
     elif user_action.startswith("show"):
             #open a file and read the content by line
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-
-            #print out the numbered items from the file
+            todos = get_todos()
+            #print out the numbered and capitalized items from the file
             for index, item in enumerate(todos):
                 item = item.strip("\n")
                 print(f"{index + 1}. {item.capitalize()}")
 
     elif user_action.startswith('replace'):
         try:
-            number = int(user_action[5:]) - 1
-
-            #open a file
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-
+            number = int(user_action[8:]) - 1
             #expect the user to enter a string for replacement
             new_todo = input("Enter new todo: ")
+            todos = get_todos()
 
             #save the new string in the same place (index)
             todos[number] = new_todo  + "\n"
             print("New Todo Has Been Saved!")
 
             #open a file and overwrite the string
-            with open("files/todos.txt", "w") as file:
-                file.writelines(todos)
+            write_todos(todos)
+
         except ValueError:
             print("Invalid Input! Please try the command again.")
             continue
@@ -46,15 +58,13 @@ while True:
         #expect the user to enter delete and index value
             number = int(user_action[7:])
             number = int(number) - 1
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-
+            todos = get_todos()
         #delete the index by the entered number. number value = index in a string
             todos.pop(number)
 
         #update the file
-            with open("files/todos.txt", "w") as file:
-                file.writelines(todos)
+            write_todos(todos)
+
         except IndexError:
             print("Invalid Input! The todo with this number does not exist.")
             continue
