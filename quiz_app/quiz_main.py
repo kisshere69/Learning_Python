f60361eb8b71ec416e0e084325ace718c5e0018f
Nikita Kiss <155293675+kisshere69@users.noreply.github.com>
2025-12-11 17:08:16 +0200

    Quiz app with an error-handling

--- quiz_app/quiz_main.py
+++ quiz_app/quiz_main.py
@@ -1,32 +1,49 @@
 # Import the json module to work with JSON files
 import json
 
-# Open the JSON file 'questions.json' in read mode
-with open("questions.json", 'r') as json_file:
-    content = json_file.read()  # Read the entire content of the file as a string
+# ----------- Load JSON with error handling ------------
+try:
+    with open("questions.json", "r") as json_file:
+        content = json_file.read()
+except FileNotFoundError:
+    print("Error: questions.json not found!")
+    exit()
 
-# Parse the JSON string into a Python object (list of dictionaries)
-data = json.loads(content)
+#Parse JSON safely to avoid incorrect JSON file syntax, structure.
+try:
+    data = json.loads(content)
+except json.JSONDecodeError:
+    print("Error: questions.json contains invalid JSON!")
+    exit()
 
-# Initialize counters for correct and incorrect answers
+# ------------ Initialize counters ---------------
 correct_answer = 0
 incorrect_answer = 0
 
 # Automatically get the total number of questions from the data
 questions = len(data)
 
-
-# Loop through each question in the data, with an index
+# ------------ Main quiz loop --------------------
 for question_index, question in enumerate(data):
-    # Print the question number and the question text
+
+    # Print the question number and the question itself
     print(f"Question {question_index + 1}) {question['question_text']}")
 
-    # Loop through all alternatives for the current question, with an index
-    for index, alternative in enumerate(question['alternatives']):
-        # Print each alternative option numbered starting from 1
-        print(f"{index + 1}) {alternative}")
+    # Loop through answer_option for the current question, with an index
+    for index, answer_option in enumerate(question['answer_option']):
+        print(f"{index + 1}) {answer_option}")
 
-    user_answer = int(input("Enter your answer number: "))
+    #Error handling try-except while-loop
+    while True:
+     try:
+        user_answer = int(input("Enter your answer number: "))
+        if user_answer < 1 or user_answer > len(question["answer_option"]):
+            print("Invalid choice! Choose an existing answer number.")
+            continue
+        break
+     except ValueError:
+        print("Invalid input. Please enter a number.")
+
     question["user_answer"] = user_answer
 
     # Check if the user's answer matches the correct answer
