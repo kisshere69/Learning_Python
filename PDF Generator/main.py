ac009320ce3fa9dd286c8b16396a2d1d846dabb4
kisshere69 <kizzfromdahood@gmail.com>
2025-12-12 14:22:51 +0200

    PDF Generator. Added for-loop for page generation and saturation with Topics and styling.

--- PDF Generator/main.py
+++
@@ -1,8 +0,0 @@
-from fpdf import FPDF
-
-pdf = FPDF(orientation='Portrait', unit='mm', format='A4')
-
-pdf.add_page()
-pdf.set_font("Arial", size=12)
-pdf.cell(0, 10, txt="Hello World!", ln=1, align="Center", border=1)
-pdf.output("output.pdf")
