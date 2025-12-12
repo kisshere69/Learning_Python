5f6555dee9611542d6401b2c6ed39ee57edd7f4e
kisshere69 <kizzfromdahood@gmail.com>
2025-12-12 14:00:13 +0200

    PDF Generator. Added for-loop for page generation and saturation with Topics and styling.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for index, row in df.iterrows():
     pdf.add_page() # Add a page for each topic
     pdf.set_font("Arial", size=12, style="B") # Set Arial Bold font for topic title
     pdf.set_text_color(0, 0, 255) # Set blue text color
-    pdf.cell(0, 10, txt=row["Topic"], ln=1, align="L", border=0, ) #Add the topic title
+    pdf.cell(0, 10, txt=row["Topic"], ln=1, align="L", border=0, ) # Add the topic title
     pdf.line(10, 20, 200, 20) # Add a line under the topic title
 
 pdf.output("output.pdf")
