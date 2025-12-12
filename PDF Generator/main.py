c886ed6d42d3346f5e1e12b96a6a5e2854a8efc4
Nikita Kiss <155293675+kisshere69@users.noreply.github.com>
2025-12-12 14:16:18 +0200

    PDF Generator
    Added a for-loop for page generation and saturation with Topics and styling.

--- PDF Generator/main.py
+++ PDF Generator/main.py
@@ -1,8 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
+
 
 pdf = FPDF(orientation='Portrait', unit='mm', format='A4')
 
-pdf.add_page()
-pdf.set_font("Arial", size=12)
-pdf.cell(0, 10, txt="Hello World!", ln=1, align="Center", border=1)
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font("Arial", size=12, style="B")
+    pdf.set_text_color(0, 0, 255)
+    pdf.cell(0, 10, txt=row["Topic"], ln=1, align="L", border=0)
+    pdf.line(10, 20, 200, 20)
 pdf.output("output.pdf")
