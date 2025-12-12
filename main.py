3c123712e2604e5627d2300dbc1e8f81c563fe14
kisshere69 <kizzfromdahood@gmail.com>
2025-12-12 05:13:03 +0200

    PDF Generator. Initial stage.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ pdf = FPDF(orientation='Portrait', unit='mm', format='A4')
 
 pdf.add_page()
 pdf.set_font("Arial", size=12)
-pdf.cell(0, 10, txt="Hello World!", ln=1, align="Center", border=1)
+pdf.cell(0, 11, txt="Hello World!", ln=1, align="Center", border=1)
 pdf.output("output.pdf")
