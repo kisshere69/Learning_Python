439872942b754fc00dbc61a45bcea1def162df5e
Nikita Kiss <155293675+kisshere69@users.noreply.github.com>
2025-12-14 13:02:29 +0200

    Web Scraping Example
    Access the link
    Extract the desired tag specified in the .yaml  file
    Write the new data in the file data.txt
    Prevent invalid data from being written into the file
    Simulate sending an email if the event contains valid data

---
+++ Web_Scraping/main.py
@@ -0,0 +1,45 @@
+import requests
+import selectorlib
+
+#Access the link as a legit user-agent
+url = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+#---Define functions---
+
+def scrape(url):
+    response = requests.get(url, HEADERS).text
+    return response
+
+def extract(response):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml").extract(response)["tours"]
+    return extractor
+
+def send_email():
+    print("Email sent.")
+
+def write(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+#---Call the functions---
+scraped = scrape(url)
+extracted = extract(scraped)
+content = read(extracted)
+
+#---Print out the result---
+print(extracted)
+
+#---Check if the extracted data is valid---
+if extracted != "No upcoming tours":
+
+    #---Check if data already exists. Send an email only if the data is new in the file--
+    if extracted not in content:
+        write(extracted)
+        send_email()
