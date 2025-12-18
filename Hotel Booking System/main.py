fac14efe19bac42db1de0131f98862fcc46be383
Nikita Kiss <155293675+kisshere69@users.noreply.github.com>
2025-12-18 17:57:37 +0200

    Hotel Booking System
    To be finished.

--- Hotel Booking System/main.py
+++ Hotel Booking System/main.py
@@ -1,7 +1,7 @@
 import pandas
-import csv
 
 df = pandas.read_csv("hotels.csv", dtype={"id":str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -12,8 +12,9 @@ class Hotel:
     def book(self): #refer to a Hotel class
 
         """
-        Book a hotel by changing its availability to 'no'
+        Book a hotel by changing its availability to 'no' in the file
         """
+
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
         df.to_csv("hotels.csv", index = False)
 
@@ -45,17 +46,37 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, full_name, cvc):
+        card_data = {
+            "number": self.number,
+            "expiration": expiration,
+            "cvc": cvc,
+            "holder": full_name
+        }
+
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
 print(df)
 
 hotel_id = input("Enter a hotel id: ")
 hotel = Hotel(hotel_id)
 
-if hotel.available:
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name = name,
-                                           hotel_object = hotel)
-    print(reservation_ticket.generate())
+if hotel.available():
+    credit_card = CreditCard(number = "123456789123123")
+    if credit_card.validate(expiration = "12/26", full_name = "JOHN SMITH", cvc = "123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name = name, hotel_object = hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("Sorry, your credit card is not valid.")
 else:
     print("Sorry, the hotel that you've selected is currently not available."
           "Don't worry, you can still select another hotel.")
